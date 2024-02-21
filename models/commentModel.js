e0d3bd03332881cc5b0bc5c3c292fff44df37d4a
Sargun <sargun.kohli152@gmail.com>
2024-02-21 14:56:42 +0530

    checking

--- models/commentModel.js
+++ models/commentModel.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
     post:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Post", //reference to the post model
+        ref: "Post"
     },
     user:{
         type: String,
-        required: true
+        required: true,
     },
     body: {
         type: String,
-        required: true,
+        required: true
     }
-});
+})
 
 module.exports = mongoose.model("Comment", commentSchema);
